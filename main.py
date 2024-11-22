4af3f4b95c868aaaa3eb907ba5dd193dc013e8be
Johan Recaman <johanstrr@gmail.com>
2024-11-22 16:32:54 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,101 @@
+
+def remove_html_tags(txt):
+    start_key = "= ["
+    end_key = "];"
+
+    start_tag_index = (txt.find(start_key) + len(start_key))
+    end_tag_index = (txt.find(end_key) - start_tag_index)
+
+    txt = txt[start_tag_index:]
+    txt = txt[:end_tag_index]
+
+    return txt
+
+def format(txt):
+    txt = txt.replace("|", ",")
+    txt = txt.replace("'", "")
+    txt = txt.replace(" ", "")
+    txt = txt.replace("\n", "")
+
+    return txt
+
+def dash_to_comma(txt):
+    result = []
+    exceptions = [":", ",", "["]
+    for i in range(len(txt)):
+        if txt[i] == "-" and txt[i-1] not in exceptions:
+            result.append(",")
+        elif txt[i] == "-" and txt[i-1] == ":" and not txt[i+1].isdigit():
+            result.append("NULL")
+        else:
+            result.append(txt[i]) 
+    return ''.join(result)
+
+def join_special_values(txt):
+    modified_lines = []
+
+    for line in txt:
+        if '(' in line:
+            start_index = line.index('(')
+            end_index = line.rindex(')') + 1
+
+            before = line[:start_index]
+            between = line[start_index:end_index]
+            after = line[end_index:]
+
+            between = between.replace("),(", ")(").replace("(", "[").replace(")", "]")
+
+            line = f"{before},{after.strip(',')},{between}"
+        modified_lines.append(line)
+    return modified_lines
+
+def set_values(txt):
+    #p1 #p2 #p3 #p4 #p5 ...  #p6
+    null = "Null"
+    updated_txt = []
+
+    for line in txt:
+        fields = line.split(',')
+        if "Alt" in fields:
+            continue
+        fields[5:5] = [null] * 25
+        updated_txt.append(','.join(fields))
+    return updated_txt
+        
+            
+
+    return txt
+def remove_keys(txt):
+    updated_txt = []
+    for line in txt:
+        fields = line.split(',')
+        values = []
+        for field in fields:
+            if ':' in field and '(' not in field:
+                values.append(field.split(':', 1)[1])
+
+            else:
+                values.append(field)
+        updated_txt.append(','.join(values))
+    return updated_txt
+
+
+def separate_by_line(txt):
+    return txt.replace("[", "").split("],")
+
+def main():
+    file = open("trace.html", "r")
+    txt = file.read()
+    file.close()
+
+    txt = remove_html_tags(txt)
+    txt = format(txt)
+    txt = dash_to_comma(txt)
+    txt = separate_by_line(txt)
+    txt = set_values(txt)
+    txt = remove_keys(txt)
+    txt = join_special_values(txt)
+
+    print(txt[1])
+if __name__ == "__main__":
+    main()
